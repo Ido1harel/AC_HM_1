614a38f589f2ed0903f1bf6fbd7c22a3c67aac8c
dangalg <dangalg@gmail.com>
2022-03-01 21:18:37 +0200

    Reorganized Code
    Removed uneeded code

--- Q1/Q1.c
+++ Q1/Q1.c
@@ -1,18 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int calcDividedDigits(int number,int digit);
+int calcDividedDigits(int number, int digit);
 
 void main()
 {
-	int mainNumber=0;
+	int mainNumber = 0;
 	int digit = 0;
 
 	printf("Please enter the main number:\n");
 	scanf("%d", &mainNumber);
 	printf("Please enter the dividing digit:\n");
 	scanf("%d", &digit);
-	printf("The result is: %d", calcDividedDigits(mainNumber,digit));
-	 
+	printf("The result is: %d", calcDividedDigits(mainNumber, digit));
+
 }
 
 int calcDividedDigits(int number, int digit)
@@ -23,9 +23,9 @@ int calcDividedDigits(int number, int digit)
 		return counter;
 	}
 
-	if ((number%10) % digit == 0)
-   	{
+	if ((number % 10) % digit == 0)
+	{
 		counter++;
-    }
+	}
 	calcDividedDigits(number / 10, digit);
 }
