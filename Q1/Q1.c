141d6e9a24eff0fdcaa9d4995ada9eb0e1704d81
dangalg <dangalg@gmail.com>
2022-03-02 20:26:49 +0200

    Finished task Q1
    Finished task Q1
    Created task Q2

--- Q1/Q1.c
+++ Q1/Q1.c
@@ -1,31 +1,47 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int calcDividedDigits(int number, int digit);
 
 void main()
-{    //   המספר הכללי
+{ 
 	int mainNumber = 0;
-	//   המספר בו יתבצע חילוק לכל ספרה בנפרד
-	int digit = 0;       
+	int digit = 0;  
+
+	// get the number from the user
 	printf("Please enter the main number:\n");
 	scanf("%d", &mainNumber);
+
+	// get the digit to divide by from the user
 	printf("Please enter the dividing digit:\n");
 	scanf("%d", &digit);
-	printf("The result is: %d", calcDividedDigits(mainNumber, digit));
 
+	// print the result
+	printf("The result is: %d", calcDividedDigits(mainNumber, digit));
 }
 
-
+/// <summary>
+/// The function is a recursion that returns the amount of numbers that are divided cleanly by the digit supplied
+/// </summary>
+/// <param name="number">The number to check</param>
+/// <param name="digit">The dividing digit</param>
+/// <returns>The amount divided cleanly</returns>
 int calcDividedDigits(int number, int digit)
 {
-	int static counter = 0; 
+	// The stopping condition of the recursion
 	if (number <= 0)
 	{
-		return counter;
+		return 0;
 	}
+
+	// if the digit is divided cleanly add 1 to the return amount
 	if ((number % 10) % digit == 0)
 	{
-		counter++;
+		return calcDividedDigits(number / 10, digit) + 1;
 	}
-	calcDividedDigits(number / 10, digit);
+	// if the digit is not divided cleanly return the amount
+	else 
+	{
+		return calcDividedDigits(number / 10, digit);
+	}
 }
